9e9e53d08c0c26e02faad595f382f9f8dacd9d6c
Michael <3533648+hertg@users.noreply.github.com>
2022-08-10 22:25:34 +0200

    comment out benchmark test

--- go.mod
+++ go.mod
@@ -9,8 +9,6 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/jaypipes/pcidb v1.0.0 // indirect
-	github.com/mitchellh/go-homedir v1.0.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/pciids/benchmark_test.go
+++ pkg/pciids/benchmark_test.go
@@ -1,54 +1,23 @@
 package pciids_test
 
-import (
-	"bufio"
-	"os"
-	"testing"
-
-	"github.com/hertg/go-pciids/pkg/pciids"
-	"github.com/jaypipes/pcidb"
-)
-
 var benchFileName = "/usr/share/hwdata/pci.ids"
 
-func BenchmarkGoPCIIDS(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		f, err := os.Open(benchFileName)
-		if err != nil {
-			b.Skipf("unable to open pci.ids file '%s' for benchmark: %s", benchFileName, err)
-		}
-		s := bufio.NewScanner(f)
-		pciids.NewDB(s)
-	}
-}
-
-func BenchmarkPCIDB(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, err := pcidb.New(&pcidb.WithOption{Path: &benchFileName})
-		if err != nil {
-			b.Skipf("unable to initialize pcidb: %s", err)
-		}
-	}
-}
-
-// func BenchmarkCustom(b *testing.B) {
+// func BenchmarkGoPCIIDS(b *testing.B) {
 // 	for i := 0; i < b.N; i++ {
-// 		f, _ := os.Open(benchFileName)
-// 		s := bufio.NewScanner(f)
-
-// 		db := &pciids.DB{
-// 			Vendors:    make(map[pciids.VendorID]pciids.Vendor),
-// 			Devices:    make(map[pciids.DeviceID]pciids.Device),
-// 			Classes:    make(map[uint8]pciids.Class),
-// 			Subsystems: make(map[pciids.SubsystemID]pciids.Subsystem),
+// 		f, err := os.Open(benchFileName)
+// 		if err != nil {
+// 			b.Skipf("unable to open pci.ids file '%s' for benchmark: %s", benchFileName, err)
 // 		}
-// 		subsystemCache := make(map[pciids.VendorID][]*pciids.Subsystem)
+// 		s := bufio.NewScanner(f)
+// 		pciids.NewDB(s)
+// 	}
+// }
 
-// 		for s.Scan() {
-// 			line := s.Bytes()
-// 			_ = db
-// 			_ = subsystemCache
-// 			_ = line
+// func BenchmarkPCIDB(b *testing.B) {
+// 	for i := 0; i < b.N; i++ {
+// 		_, err := pcidb.New(&pcidb.WithOption{Path: &benchFileName})
+// 		if err != nil {
+// 			b.Skipf("unable to initialize pcidb: %s", err)
 // 		}
 // 	}
 // }
